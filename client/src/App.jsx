c8a036374c4b3e1f5396e16664b741254c1d2ef6
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-07-24 15:14:22 +0200

    Add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 // Composant ErrorBoundary pour capturer les erreurs
 /* class ErrorBoundary extends React.Component {
@@ -45,6 +46,7 @@ export default function App() {
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     
   );
