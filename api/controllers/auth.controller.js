4ec11a70be74ca8c170cd35645c1f16e0d519088
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-07-24 16:06:42 +0200

    Create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,27 +1,31 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-//import jwt from "jsonwebtoken";
-import { errorHandler } from '../utils/error.js'; // Assure-toi que ce module existe et est bien importé
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  // Validation des champs requis
-  if (!username || !email || !password || username === "" || email === "" || password === "") {
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
     return next(errorHandler(400, "All fields are required"));
   }
 
   try {
-    // Vérifier si l'utilisateur existe déjà
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return next(errorHandler(400, "L'utilisateur existe déjà !"));
     }
 
-    // Hachage du mot de passe
     const hashedPassword = bcryptjs.hashSync(password, 10);
+    console.log("Hashed Password:", hashedPassword);
 
-    // Création d'un nouvel utilisateur
     const newUser = new User({
       username,
       email,
@@ -38,39 +42,32 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  // Validation des champs requis
   if (!email || !password || email === "" || password === "") {
-    return next(errorHandler(400, "All fields are required"));
+    next(errorHandler(400, "All fields are required"));
   }
 
   try {
-    // Trouver l'utilisateur par e-mail
     const validUser = await User.findOne({ email });
     if (!validUser) {
       return next(errorHandler(404, "User not found"));
     }
-
-    // Vérifier le mot de passe
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compare(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-
-    // Génération du token JWT
     const token = jwt.sign(
       { id: validUser._id, isAdmin: validUser.isAdmin },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' } // Ajouter une expiration au token pour plus de sécurité
+      process.env.JWT_SECRET
     );
 
-    // Réponse avec le token
-    const { password: pass, ...userData } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
+
     res
       .status(200)
       .cookie("access_token", token, {
         httpOnly: true,
       })
-      .json(userData);
+      .json(rest);
   } catch (error) {
     next(error);
   }
@@ -80,15 +77,13 @@ export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;
 
   try {
-    // Trouver l'utilisateur par e-mail
     let user = await User.findOne({ email });
 
     if (user) {
-      // Générer un token JWT pour l'utilisateur existant
       const token = jwt.sign(
         { id: user._id, isAdmin: user.isAdmin },
         process.env.JWT_SECRET,
-        { expiresIn: '1h' } // Ajouter une expiration au token pour plus de sécurité
+        { expiresIn: "1h" }
       );
       const { password, ...userData } = user._doc;
       res
@@ -98,22 +93,24 @@ export const google = async (req, res, next) => {
         })
         .json(userData);
     } else {
-      // Créer un nouvel utilisateur pour les connexions Google
-      const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       user = new User({
-        username: name.toLowerCase().replace(/\s+/g, '') + Math.random().toString(9).slice(-4),
+        username:
+          name.toLowerCase().replace(/\s+/g, "") +
+          Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
         profilePicture: googlePhotoUrl,
       });
       await user.save();
 
-      // Générer un token JWT pour le nouvel utilisateur
       const token = jwt.sign(
         { id: user._id, isAdmin: user.isAdmin },
         process.env.JWT_SECRET,
-        { expiresIn: '1h' } // Ajouter une expiration au token pour plus de sécurité
+        { expiresIn: "1h" }
       );
       const { password: pass, ...userData } = user._doc;
       res
