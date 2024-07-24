c634a0413b41fbccbfcd65b177939b02e226022d
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-07-24 16:39:26 +0200

    install redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,29 +1,29 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-/* import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   signInStart,
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import OAuth from "../components/OAuth"; */
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  //const { loading, error: errorMessage } = useSelector((state) => state.user);
-  //const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-   /*  if (!formData.email || !formData.password) {
+    if (!formData.email || !formData.password) {
       return dispatch(signInFailure("Please fill all the fields"));
-    } */
+    }
     try {
-     // dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -31,15 +31,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-       // dispatch(signInFailure(data.message));
+        dispatch(signInFailure(data.message));
       }
 
       if (res.ok) {
-       // dispatch(signInSuccess(data));
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-    //  dispatch(signInFailure(error.message));
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -49,9 +49,8 @@ export default function SignIn() {
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-             PopCultureQuest
+              PopCultureQuest
             </span>
-            
           </Link>
           <p className="text-sm mt-5">
             This is a demo project. You can sign in with your email and password
@@ -83,18 +82,19 @@ export default function SignIn() {
             <Button
               gradientDuoTone="purpleToPink"
               type="submit"
-             // disabled={loading}
+              disabled={loading}
             >
-             {/*  {loading ? (
+              {loading ? (
                 <>
                   <Spinner size="sm" />
                   <span className="pl-3">Loading...</span>
                 </>
               ) : (
                 "Sign In"
-              )} */} coucou
+              )}{" "}
+              coucou
             </Button>
-           {/*  <OAuth /> */}
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
@@ -102,11 +102,11 @@ export default function SignIn() {
               Sign Up
             </Link>
           </div>
-         {/*  {errorMessage && (
+          {errorMessage && (
             <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
