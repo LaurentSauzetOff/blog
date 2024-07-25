bc62064962a7c02329fe7f46ac2706caa29e60cb
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-07-25 11:09:17 +0200

    Dashboard privacy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 // Composant ErrorBoundary pour capturer les erreurs
 /* class ErrorBoundary extends React.Component {
@@ -35,19 +35,19 @@ import Footer from './components/Footer';
 
 export default function App() {
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
