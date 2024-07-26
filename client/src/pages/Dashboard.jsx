646a70fcd8cd58936487f98b563efd3669d2a2dc
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-07-26 12:19:19 +0200

    posts dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
-import { useEffect,  useState  } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
-// import DashPosts from '../components/DashPosts';
+import DashPosts from "../components/DashPosts";
 // import DashUsers from '../components/DashUsers';
 // import DashComments from '../components/DashComments';
 // import DashboardComp from '../components/DashboardComp';
@@ -13,9 +13,9 @@ export default function Dashboard() {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-      if (tabFromUrl) {
+    if (tabFromUrl) {
       setTab(tabFromUrl);
-    } 
+    }
   }, [location.search]);
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
@@ -26,7 +26,7 @@ export default function Dashboard() {
       {/* profile... */}
       {tab === "profile" && <DashProfile />}
       {/* posts... */}
-      {/*  {tab === 'posts' && <DashPosts />} */}
+      {tab === "posts" && <DashPosts />}
       {/* users */}
       {/*   {tab === 'users' && <DashUsers />} */}
       {/* comments  */}
